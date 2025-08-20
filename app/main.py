a593f39e905af6133fd33723b4316b7f67fddde0
Maycon Douglas <mdrs.maycon@gmail.com>
2025-08-20 12:46:42 -0300

    feat: refatora o tratamento API_PREFIX e atualiza o esquema de autenticação para usar HTTPBearer via swagger

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
+from app.config import API_PREFIX
 from app.exceptions.exception_handlers import register_exception_handlers
 from app.routes.chat_router import chat_router
 from app.routes.auth_router import auth_router
 from app.routes.paint_router import paint_router
 from app.routes.user_router import user_router
 
-API_PREFIX = "/api/v1"
 
 app = FastAPI()
 register_exception_handlers(app)
